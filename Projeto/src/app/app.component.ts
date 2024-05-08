68153fab4d84a3be46ff67f83ddc782233421f40
Wendel de Andrade <shindagami@gmail.com>
2024-05-08 01:50:55 -0300

    feat(Tela Inicial): Montagem do esqueleto da tela inicial

---
+++ Projeto/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './componentes/header/header.component';
+import { SideBarComponent } from './componentes/side-bar/side-bar.component';
+import { MainComponent } from './componentes/main/main.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, SideBarComponent, MainComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Projeto';
+}
