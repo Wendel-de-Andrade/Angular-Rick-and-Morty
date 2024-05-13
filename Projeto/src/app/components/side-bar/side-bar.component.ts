80e763719f5191186698597a84019cc8abb8fde4
Wendel de Andrade <shindagami@gmail.com>
2024-05-12 21:45:49 -0300

    style(sidebar): Modificando o estilo da sidebar

--- Projeto/src/app/components/side-bar/side-bar.component.ts
+++ Projeto/src/app/components/side-bar/side-bar.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
